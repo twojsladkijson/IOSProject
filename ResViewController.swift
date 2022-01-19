9f03a078ca1d857788934e9d3b372db71751ceae
Dima <97967773+twojsladkijson@users.noreply.github.com>
2022-01-19 18:49:47 +0300

    IOSProject

--- ResViewController.swift
+++ ResViewController.swift
@@ -3,12 +3,12 @@ import UIKit
 class ResultViewController: UIViewControler {
     var reslabe1Title = ""
 
-    @IBAction func recalcPressed(_ sender: UIButton){
-        self.dismiss(animated: true, completion: nil)
+    @IBAction func recalcPressed(_ sender: UIButton){     //конектим кнопку "Пересчитать"
+        self.dismiss(animated: true, completion: nil)     //что бы это экран у нас пропар 
     }
-    @IBAOutLet weak var resLabe1: UILabe1!
+    @IBAOutLet weak var resLabe1: UILabe1!                //конектим лейбл где будет нащ результат 
     override func viewDidload(){
         super.viewDidload()
-        resLabe1.text = reslabe1Title
+        resLabe1.text = reslabe1Title                     //Делаем так что бы он выводил результат который к нас получился 
     }
 }
